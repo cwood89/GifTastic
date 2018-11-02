87d11f388f7abd2a2ee96bba68d2968e149a54c4
woodtop <cwood89@mac.com>
2018-11-02 16:06:55 -0400

    almost done

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-// api key = 
-// 
-
-var topics = ["basketball", "kobe", "lebron", "skateboarding", "kickflip", "balisong", "pokemon", "zelda"];
+var topics = ["basketball", "kobe", "lebron", "skateboarding", "kickflip", "butterfly knife", "balisong", "pokemon", "zelda"];
 
 function addButtons() {
+    // empty button div
+    $("#buttons").empty();
     // loops through topics array
     for (var i = 0; i < topics.length; i++) {
-        
+        // create button element
         var b = $("<button>");
         // adds a class to buttons for styling
         b.addClass("topic");
@@ -26,21 +25,66 @@ console.log(topics);
 $("#addGif").on("click", function(event) {
     event.preventDefault();
     var gifAdd = $("#gif-input").val().trim();
-    $("#buttons").empty();
     topics.push(gifAdd);
     addButtons();
 });
+
 // run gif search on button click
 $("button").on("click", function() {
+    // empty gif display div
+    $("#gif-display").empty();
     // obtain value from button
     var searchName =  $(this).attr("data-name");
     // create url for api
     var key = "0XrbbyngP4RtQh2ck5pNTozg6RlDTD31";
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchName 
     + "&api_key=" + key + "&limit=10";
-// api call
+    // api call
     $.ajax({
         url: queryURL,
         method: "GET"
-      })
-    .then(function(response) {
+    }).then(function(response) {
+        var results = response.data;
+        console.log(results);
+        // loop through all results
+        for (var i = 0; i < results.length; i++) {
+            // create gif holder div
+            var gifDiv = $("<div>");
+            // create image element
+            var image = $("<img>")
+            // add api data to image element
+            image.attr("data-animate", results[i].images.fixed_height.url);
+            image.attr("data-still",  results[i].images.fixed_height_still.url);
+            image.attr("src", image.attr("data-still"));
+            image.attr("data-state", "still");
+            // on click run playPause function
+            image.on("click", playPause)
+            // rating display
+            var rating = $("<p>");
+            // get rating from api call
+            rating.text("Rating: " + results[i].rating);
+            // add data to div
+            gifDiv.append(image);
+            gifDiv.append(rating);
+            // append div to DOM
+            $("#gif-display").append(gifDiv);
+            
+        };
+    });
+});
+
+// play pause function
+function playPause() {
+    var state = $(this).attr("data-state");
+    console.log(state);
+    // if gif is sill then amimate
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    // if gif is amimated then pause
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+      }
+};
+
